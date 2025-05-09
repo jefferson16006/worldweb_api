a0e7e61100100703e5c7456a23ef15e4ebdc585b
Jefferson <jeffersonchukwu2006@gmail.com>
2025-05-09 16:18:42 +0100

    Enabled uptime

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 //routes
 app.get('/', (req, res) => {
-    res.send('Home route')
+    console.log('Server pinged.')
 })
 app.use('/api/auth', authRouter)
 app.use('/api', aiRoutes);
